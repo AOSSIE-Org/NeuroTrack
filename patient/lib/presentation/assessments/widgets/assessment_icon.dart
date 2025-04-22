52d114ff498602bd77b97c98f9210df15a2a6e5d
TriptiMirani <tripti.mirani2@gmail.com>
2025-04-22 18:03:43 +0530

    fix: assessment list screen icons rendering

--- patient/lib/presentation/assessments/widgets/assessment_icon.dart
+++ patient/lib/presentation/assessments/widgets/assessment_icon.dart
@@ -10,11 +10,27 @@ class AssessmentIcon extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Image.network(
-      icon,
-      width: 80,
-      height: 80,
-      fit: BoxFit.contain,
-    );
+    // Check if the icon is a network URL or an asset path
+    if (icon.startsWith('http')) {
+      return Image.network(
+        icon,
+        width: 80,
+        height: 80,
+        fit: BoxFit.contain,
+        errorBuilder: (context, error, stackTrace) {
+          return const Icon(Icons.error_outline, size: 80);
+        },
+      );
+    } else {
+      return Image.asset(
+        icon,
+        width: 80,
+        height: 80,
+        fit: BoxFit.contain,
+        errorBuilder: (context, error, stackTrace) {
+          return const Icon(Icons.error_outline, size: 80);
+        },
+      );
+    }
   }
 }
